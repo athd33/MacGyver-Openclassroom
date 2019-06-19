d8c914ad49b68cf88e5a48d9d636285730a4a880
Scope75 <scope@gmx.fr>
2019-06-19 14:48:43 +0200

    update

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
-import sys
+"""Setup file to create a exe"""
+
+import os
 from cx_Freeze import setup, Executable
 
-setup(name="macgyver.py",
-      version="0.1",
-      description="MacGyver game",
-      options = {'build_exe': {'include_files':["./src/"]}}, 
-executables=[Executable( "main.py")])
+
+
+# call setup  and add files / folder
+setup(
+    name = "MacGyver escape game",
+    version = "1.00",
+    options= {"build_exe": {"packages": ["pygame"],
+                            "include_files": ["sounds", "images", "constants.py", "fonctions.py", "items.py","player.py","mapp.txt","mapp.py","player.py"]}},		
+    description = "Projet 03 Labyrinth Game",
+    executables = [Executable("main.py")],
+)
